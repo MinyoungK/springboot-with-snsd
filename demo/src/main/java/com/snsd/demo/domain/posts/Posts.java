c12a5291632eeaceb6a22cb64c88568fa350f1af
MinyoungK <ddorogi00@gmail.com>
2023-06-18 02:36:23 +0900

    JPA Auditing으로 생성시간/수정시간 자동화

--- demo/src/main/java/com/snsd/demo/domain/posts/Posts.java
+++ demo/src/main/java/com/snsd/demo/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.snsd.demo.domain.posts;
 
+import com.snsd.demo.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import jakarta.persistence.*;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

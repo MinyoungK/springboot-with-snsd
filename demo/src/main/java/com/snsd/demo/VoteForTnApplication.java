5a13966ba71801c5fe75d7f0a578692ff0113233
MinyoungK <ddorogi00@gmail.com>
2023-06-17 15:09:57 +0900

    프로젝트 생성

---
+++ demo/src/main/java/com/snsd/demo/VoteForTnApplication.java
@@ -0,0 +1,13 @@
+package com.snsd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteForTnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteForTnApplication.class, args);
+    }
+
+}
---
+++ demo/src/test/java/com/snsd/demo/VoteForTnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snsd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteForTnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
